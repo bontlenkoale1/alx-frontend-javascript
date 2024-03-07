ed42403ef56cb8c44d42aba1eb5679391fc4d8d1
root <bontlenkoale20@gmail.com>
2024-03-07 19:09:40 +0000

    ES6_classes

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,34 +1,42 @@
-// eslint-disable-next-line
-// implementing static method
-import Currency from "./3-currency";
+// Import the Currency class from '3-currency.js'
+import Currency from './3-currency.js';
 
-export default class Pricing {
+class Pricing {
+  // Constructor with amount and currency parameters
   constructor(amount, currency) {
-    this.amount = amount;
-    this.currency = currency;
+    this._amount = amount;
+    this._currency = currency;
   }
 
+  // Getter for the amount
   get amount() {
     return this._amount;
   }
 
-  set amount(value) {
-    this._amount = value;
+  // Setter for the amount
+  set amount(newAmount) {
+    this._amount = newAmount;
   }
 
+  // Getter for the currency
   get currency() {
     return this._currency;
   }
 
-  set currency(value) {
-    this._currency = value;
+  // Setter for the currency
+  set currency(newCurrency) {
+    this._currency = newCurrency;
   }
 
+  // Method to display the full price in the requested format
   displayFullPrice() {
-    return `${this.amount} ${this.currency.displayFullCurrency()}`;
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
   }
 
+  // Static method to convert the price given an amount and a conversion rate
   static convertPrice(amount, conversionRate) {
     return amount * conversionRate;
   }
 }
+
+export default Pricing;
